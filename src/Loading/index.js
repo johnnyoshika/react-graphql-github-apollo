94bd39884f1067b33b43cab0fc3bd776f5395891
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-09 11:55:15 -0800

    Animate Loading component

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './style.css';
 
+const TICK_RATE = 500;
+
 export default ({ isCenter }) => {
+  const [dots, setDots] = useState(0);
+
+  const onTick = () => setDots(dots => dots + 1);
+
+  useEffect(() => {
+    const interval = setInterval(onTick, TICK_RATE);
+    return () => clearInterval(interval);
+  }, []);
+
   const classNames = ['Loading'];
   if (isCenter) classNames.push('Loading_center');
 
-  return <div className={classNames.join(' ')}>Loading...</div>;
+  return <div className={classNames.join(' ')}>Loading {new Array(dots % 4).fill(0).map(dot => '.')}</div>;
 };
