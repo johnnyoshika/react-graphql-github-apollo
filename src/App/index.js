041bafe72ddc47e2c0b2e0139e9765b3297cddd0
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-09 12:08:34 -0800

    Use router to navigate between Organization and Profile

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from './Navigation';
+import Organization from '../Organization';
 import Profile from '../Profile';
 
+import * as routes from '../constants/routes';
+
+import './style.css';
+
 export default () => (
-  <Profile />
+  <Router>
+    <div className="App">
+      <Navigation />
+      <div className="App-main">
+        <Route
+          exact
+          path={routes.ORGANIZATION}
+          component={() => (
+            <div className="App-content_large-header">
+              <Organization />
+            </div>
+          )}
+        />
+        <Route
+          exact
+          path={routes.PROFILE}
+          component={() => (
+            <div className="App-content_small-header">
+              <Profile />
+            </div>
+          )}
+        />
+      </div>
+    </div>
+  </Router>
 );
