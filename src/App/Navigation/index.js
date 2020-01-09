2f846af3417024240a29b5471c0378e35ae1498e
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-09 15:38:55 -0800

    Use React Router's higher-order component to pass in current URL

--- src/App/Navigation/index.js
+++ src/App/Navigation/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import * as routes from '../../constants/routes';
 
 import './style.css';
 
-const Navigation = () => (
+const Navigation = ({
+  location: { pathname }
+}) => (
   <header className="Navigation">
     <div className="Navigation-link">
       <Link to={routes.PROFILE}>Profile</Link>
@@ -13,7 +15,10 @@ const Navigation = () => (
     <div className="Navigation-link">
       <Link to={routes.ORGANIZATION}>Organization</Link>
     </div>
+    {pathname === routes.ORGANIZATION && (
+      <span style={{color: 'white'}}>Future search field</span>
+    )}
   </header>
 );
 
-export default Navigation;
+export default withRouter(Navigation);
