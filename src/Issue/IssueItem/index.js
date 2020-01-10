53b557d80f09c3c95cafbfe0bc56ac765f1b2423
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-10 09:26:23 -0800

    Add toggle to show/hide comments

--- src/Issue/IssueItem/index.js
+++ src/Issue/IssueItem/index.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Button from '../../Button';
 import Link from '../../Link';
 import Comments from '../../Comment';
 
 import './style.css';
 
-const IssueItem = ({ issue, repositoryOwner, repositoryName }) => (
-  <div className="IssueItem">
-    {/* placeholder to add a show/hide comment button later */}
+const IssueItem = ({ issue, repositoryOwner, repositoryName }) => {
+  const [showComments, setShowComments] = useState(false);
 
-    <div className="IssueItem-content">
-      <h3>
-        <Link href={issue.url}>{issue.title}</Link>
-      </h3>
-      <div dangerouslySetInnerHTML={{ __html: issue.bodyHTML }} />
+  return (
+    <div className="IssueItem">
+      <Button onClick={() => setShowComments(!showComments)}>
+        {showComments ? '-' : '+'}
+      </Button>
 
-      <Comments
-        repositoryOwner={repositoryOwner}
-        repositoryName={repositoryName}
-        issueNumber={issue.number} />
+      <div className="IssueItem-content">
+        <h3>
+          <Link href={issue.url}>{issue.title}</Link>
+        </h3>
+        <div dangerouslySetInnerHTML={{ __html: issue.bodyHTML }} />
+
+        {showComments && (
+          <Comments
+            repositoryOwner={repositoryOwner}
+            repositoryName={repositoryName}
+            issueNumber={issue.number} />
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default IssueItem;
