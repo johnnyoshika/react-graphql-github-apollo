5716e35a8130c9bf6146a3735971acf87a7f2d31
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-10 10:35:39 -0800

    Prefetch comments on button hover

--- src/Issue/IssueItem/index.js
+++ src/Issue/IssueItem/index.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
+import { ApolloConsumer } from 'react-apollo';
 
 import Button from '../../Button';
 import Link from '../../Link';
 import Comments from '../../Comment';
 
+import { GET_COMMENTS_OF_ISSUE } from '../../Comment/CommentList/queries';
+
 import './style.css';
+
+const prefetchComments = (
+  client,
+  repositoryOwner,
+  repositoryName,
+  issueNumber
+) => client.query({
+  query: GET_COMMENTS_OF_ISSUE,
+  variables: {
+    repositoryOwner,
+    repositoryName,
+    issueNumber
+  }
+});
 
 const IssueItem = ({ issue, repositoryOwner, repositoryName }) => {
   const [showComments, setShowComments] = useState(false);
 
   return (
     <div className="IssueItem">
-      <CommentsToggle showComments={showComments} setShowComments={setShowComments} />
+      <CommentsToggle
+        showComments={showComments}
+        setShowComments={setShowComments}
+        repositoryOwner={repositoryOwner}
+        repositoryName={repositoryName}
+        issueNumber={issue.number}
+      />
 
       <div className="IssueItem-content">
         <h3>
@@ -32,11 +55,26 @@ const IssueItem = ({ issue, repositoryOwner, repositoryName }) => {
 
 const CommentsToggle = ({
   showComments,
-  setShowComments
+  setShowComments,
+  repositoryOwner,
+  repositoryName,
+  issueNumber
 }) => (
-  <Button onClick={() => setShowComments(!showComments)}>
-    {showComments ? '-' : '+'}
-  </Button>
+  <ApolloConsumer>
+    {client => (
+      <Button
+        onClick={() => setShowComments(!showComments)}
+        onMouseOver={() => prefetchComments(
+          client,
+          repositoryOwner,
+          repositoryName,
+          issueNumber
+        )}
+      >
+        {showComments ? '-' : '+'}
+      </Button>
+    )}
+  </ApolloConsumer>
 );
 
 export default IssueItem;
