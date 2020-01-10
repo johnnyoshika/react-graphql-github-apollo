78e806d3fdbbfdfb5bd8ff398edf3b5603564377
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-09 21:44:23 -0800

    Eagerly show first 5 issues for each repository
    This creates lots of http requests

--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FetchMore from '../../FetchMore';
 import RepositoryItem from '../RepositoryItem';
+import Issues from '../../Issue';
 
 import '../style.css';
 
@@ -29,6 +30,11 @@ const RepositoryList = ({ repositories, loading, fetchMore, entry }) => (
     {repositories.edges.map(({ node }) => (
       <div key={node.id} className="RepositoryItem">
         <RepositoryItem {...node} />
+
+        <Issues
+          repositoryName={node.name}
+          repositoryOwner={node.owner.login}
+        />
       </div>
     ))}
 
